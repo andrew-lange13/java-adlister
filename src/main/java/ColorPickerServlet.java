73693c1641d816014611b2180adcc4f6709175af
Andrew Lange <andrew.lange13@gmail.com>
2022-04-01 14:46:50 -0500

    adds methods to color picker servlet

--- src/main/java/ColorPickerServlet.java
+++ src/main/java/ColorPickerServlet.java
@@ -1,2 +1,23 @@
-public class ColorPickerServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ColorPickerServlet", urlPatterns = "/color-picker")
+public class ColorPickerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("colorPicker.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String color = req.getParameter("color");
+
+        System.out.println(color);
+
+        resp.sendRedirect("/color-picker");
+    }
 }
