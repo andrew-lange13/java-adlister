12f298aa16391dc162d09b7e888cae7ec96840de
Andrew Lange <andrew.lange13@gmail.com>
2022-04-01 11:51:53 -0500

    1

--- src/main/java/PizzaServlet.java
+++ src/main/java/PizzaServlet.java
@@ -5,9 +5,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 
 @WebServlet(name = "PizzaServlet", urlPatterns = "/pizza-order")
 public class PizzaServlet extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         RequestDispatcher dispatcher = request.getRequestDispatcher("/pizza.jsp");
         dispatcher.forward(request, response);
@@ -15,6 +18,24 @@ public class PizzaServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String crust = req.getParameter("crust");
+        String sauce = req.getParameter("sauce");
+        String size = req.getParameter("size");
+        String[] toppings = req.getParameterValues("toppings");
+        String address = req.getParameter("address");
+
+        System.out.println(crust);
+        System.out.println(sauce);
+        System.out.println(size);
+        System.out.println(Arrays.toString(toppings));
+
+        req.setAttribute("crust", crust);
+        req.setAttribute("sauce", sauce);
+        req.setAttribute("size", size);
+        req.setAttribute("toppings", Arrays.toString(toppings));
+        req.setAttribute("address", address);
+        req.getRequestDispatcher("/pizza.jsp").forward(req, resp);
+
+
     }
 }
---
+++ target/adlister-1.0-SNAPSHOT/login.jsp
@@ -0,0 +1,41 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<%
+    if (request.getMethod().equalsIgnoreCase("post")){
+        String username = request.getParameter("uname");
+        String password = request.getParameter("psw");
+        if (username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("password")){
+            response.sendRedirect("/profile");
+        }
+    }
+%>
+<html>
+<head>
+    <jsp:include page="partials/head.jsp">
+        <jsp:param name="title" value="Please Log In" />
+    </jsp:include>
+    <title>Login</title>
+</head>
+<body>
+<%@include file="partials/navbar.jsp"%>
+<h1>LOGIN</h1>
+<form action="login.jsp" method="post">
+    <div>
+        <label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+        </label>
+    </div>
+    <br>
+    <div>
+        <label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+        </label>
+    </div>
+    <br>
+    <div>
+        <button type="submit">Login</button>
+    </div>
+</form>
+</body>
+</html>
