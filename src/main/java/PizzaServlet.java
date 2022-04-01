48274cd70c3adb34a9be2dd79d12070e34f02f85
Andrew Lange <andrew.lange13@gmail.com>
2022-04-01 09:12:52 -0500

    adds to pizza servlet

--- src/main/java/PizzaServlet.java
+++ src/main/java/PizzaServlet.java
@@ -1,2 +1,15 @@
-public class PizzaServlet {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "PizzaServlet", urlPatterns = "/pizza-order")
+public class PizzaServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/pizza.jsp");
+        dispatcher.forward(request, response);
+    }
 }
