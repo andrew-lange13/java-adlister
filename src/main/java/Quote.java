4c3c06c5b623ee70b517978ea7319474320e3865
Andrew Lange <andrew.lange13@gmail.com>
2022-04-04 11:08:05 -0500

    adds java beans exercise

---
+++ src/main/java/Quote.java
@@ -0,0 +1,35 @@
+import java.io.Serializable;
+
+public class Quote implements Serializable {
+
+    private int id;
+    private String author;
+    private String content;
+
+    public Quote() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
