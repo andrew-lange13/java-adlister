d51bd5e3bec4c35eebf68dd5542256ae204b2412
Andrew Lange <andrew.lange13@gmail.com>
2022-04-04 13:43:07 -0500

    fixes url pattern

--- src/main/java/LoseGuessServlet.java
+++ src/main/java/LoseGuessServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "LoseGuessServlet", urlPatterns = "lose")
+@WebServlet(name = "LoseGuessServlet", urlPatterns = "/lose")
 public class LoseGuessServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
