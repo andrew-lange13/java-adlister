176bb7ba6ee393937acd7fb721ae029fae3257e6
Andrew Lange <andrew.lange13@gmail.com>
2022-04-05 11:19:30 -0500

    adds get session method for username

--- src/main/java/controllers/ViewProfileServlet.java
+++ src/main/java/controllers/ViewProfileServlet.java
@@ -5,11 +5,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String user = (String) session.getAttribute("username");
         request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
 }
