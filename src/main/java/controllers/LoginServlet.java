cc2912c4fb4c4ab6cbbeb14ac4515e8dea14b94f
Andrew Lange <andrew.lange13@gmail.com>
2022-04-07 17:07:25 -0500

    adds todos from exercise

--- src/main/java/controllers/LoginServlet.java
+++ src/main/java/controllers/LoginServlet.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import dao.DaoFactory;
+import models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
@@ -21,9 +25,20 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean login = username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("password");
 
-        if (login) {
+        // TODO: find a record in your database that matches the submitted password
+        // TODO: make sure we find a user with that username
+        // TODO: check the submitted password against what you have in your database
+        boolean validAttempt = false;
+
+        try {
+            User user = DaoFactory.getUsersDao().findByUsername(username);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (validAttempt) {
+            // TODO: store the logged in user object in the session, instead of just the username
             request.getSession().setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
