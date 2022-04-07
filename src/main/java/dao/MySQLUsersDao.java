b3ea7045f5ddfcc081c7c1420866a8c93b0238b5
Andrew Lange <andrew.lange13@gmail.com>
2022-04-07 16:15:00 -0500

    implements insert user

--- src/main/java/dao/MySQLUsersDao.java
+++ src/main/java/dao/MySQLUsersDao.java
@@ -50,6 +50,20 @@ public class MySQLUsersDao implements Users {
 
     @Override
     public Long insert(User user) {
+        String userInsert = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        try{
+            PreparedStatement stmt = connection.prepareStatement(userInsert, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 }
