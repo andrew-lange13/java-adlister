dfe1bbe3c86793f0408a2bb12a29f35c3c12561a
Andrew Lange <andrew.lange13@gmail.com>
2022-04-07 09:41:37 -0500

    adds content from exercise branch

--- src/main/java/controllers/RegisterServlet.java
+++ src/main/java/controllers/RegisterServlet.java
@@ -1,4 +1,19 @@
 package controllers;
 
-public class RegisterServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
+public class RegisterServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        // TODO: show the registration form
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        // TODO: ensure the submitted information is valid
+        // TODO: create a new user based off of the submitted information
+        // TODO: if a user was successfully created, send them to their profile
+    }
 }
