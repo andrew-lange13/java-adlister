3d6112bb482cb96642f1f2c1672cd7dbc068c84d
Andrew Lange <andrew.lange13@gmail.com>
2022-04-07 17:08:04 -0500

    adds logic

--- src/main/java/controllers/RegisterServlet.java
+++ src/main/java/controllers/RegisterServlet.java
@@ -1,19 +1,53 @@
 package controllers;
 
+import dao.DaoFactory;
+import models.User;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO: show the registration form
+        request.getRequestDispatcher("WEB-INF/register.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO: ensure the submitted information is valid
         // TODO: create a new user based off of the submitted information
         // TODO: if a user was successfully created, send them to their profile
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String confirmPassword = request.getParameter("confirm-password");
+
+        boolean valid = username.isEmpty()
+                || email.isEmpty()
+                || password.isEmpty()
+                || confirmPassword.isEmpty()
+                || !password.equals(confirmPassword);
+
+        if (valid){
+            response.sendRedirect("/register");
+        } else{
+            User user = new User();
+
+            user.setUsername(username);
+            user.setEmail(email);
+            user.setPassword(password);
+
+            try {
+                DaoFactory.getUsersDao().insert(user);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            response.sendRedirect("/profile");
+        }
     }
 }
