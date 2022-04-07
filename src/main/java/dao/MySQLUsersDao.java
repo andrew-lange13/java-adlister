c04b55c7b4defe0272ea652ed9a2c1448a947ec5
Andrew Lange <andrew.lange13@gmail.com>
2022-04-07 15:01:51 -0500

    adds jdbc connection

--- src/main/java/dao/MySQLUsersDao.java
+++ src/main/java/dao/MySQLUsersDao.java
@@ -1,4 +1,36 @@
 package dao;
 
+import com.mysql.jdbc.Driver;
+import controllers.Config;
+import models.User;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MySQLUsersDao implements Users {
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            Connection connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Long insert(User user) {
+        return null;
+    }
 }
