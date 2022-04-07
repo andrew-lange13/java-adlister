c758944cae40f3adb31d82582093c880a39abddc
Andrew Lange <andrew.lange13@gmail.com>
2022-04-06 21:06:41 -0500

    implements ads interface

--- src/main/java/dao/MySQLAdsDao.java
+++ src/main/java/dao/MySQLAdsDao.java
@@ -1,26 +1,70 @@
 package dao;
 
+import com.mysql.jdbc.Driver;
 import controllers.Config;
 import models.Ad;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MySQLAdsDao implements Ads{
 
-    private Connection connection;
+    private final Connection connection;
 
-    public MySQLAdsDao(Config config){
-
+    public MySQLAdsDao(Config config) throws SQLException {
+        DriverManager.registerDriver(new Driver());
+        connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUser(),
+                config.getPassword()
+        );
     }
 
     @Override
-    public List<Ad> all() {
-        return null;
+    public List<Ad> all(){
+        String searchString = "SELECT * FROM ads";
+        List<Ad> ads = new ArrayList<>();
+
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(searchString);
+
+            while (rs.next()){
+                ads.add(new Ad(
+                        rs.getInt("user_id"),
+                        rs.getString("title"),
+                        rs.getString("description")
+                ));
+
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return ads;
     }
 
     @Override
     public Long insert(Ad ad) {
+        String insertString = String.format(
+                "INSERT INTO ads (id, user_id, title, description) values (%d, %d, '%s', '%s')",
+                ad.getId(),
+                1,
+                ad.getTitle(),
+                ad.getDescription()
+        );
+
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(insertString, Statement.RETURN_GENERATED_KEYS);
+
+            ResultSet rs = stmt.getGeneratedKeys();
+
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
